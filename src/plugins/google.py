ca1ee6954bee6b8d8b434593f6870c610235d335
jtuu <joona.tuukkanen@gmail.com>
2018-03-24 15:40:58 +0200

    Added !google command.

---
+++ src/plugins/google.py
@@ -0,0 +1,46 @@
+import re
+import json
+import os
+import urllib.request
+from discord import Embed
+from ..utils import querify
+
+trigger = re.compile("^!g(?:oogle)?")
+
+match_pattern = re.compile(r"^!g(?:oogle)?\s+(.+)")
+base_url = "https://www.googleapis.com/customsearch/v1"
+timeout_ms = 10 * 1000
+default_params = json.load(open(os.path.abspath(os.path.dirname(__file__) + "/google.json")))
+default_params["prettyPrint"] = "false"
+default_params["fields"] = "items(title,link)"
+default_params["num"] = 3
+
+max_title_length = 80
+
+def format_response(response):
+    items = response.get("items")
+    if items:
+        embed = Embed()
+        for item in items:
+            embed.add_field(name=item["title"][:max_title_length], value=item["link"])
+        return embed
+
+async def action(bot, msg):
+    match = match_pattern.match(msg.clean_content)
+    if match:
+        params = dict(default_params)
+        params["q"] = match.groups()[0]
+
+        try:
+            response = urllib.request.urlopen(base_url + querify(params)).read()
+        except urllib.error.HTTPError as err:
+            print("Google search request failed: %s" % err)
+        else:
+            try:
+                parsed = json.loads(response)
+            except json.JSONDecodeError as err:
+                print("Failed to parse Google search response: %s" % err)
+            else:
+                formatted = format_response(parsed)
+                if formatted:
+                    await bot.send_message(msg.channel, embed=format_response(parsed))
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from random import randint
+import urllib.parse
 
 def discord_snowflake_to_datetime(snowflake):
     return datetime.fromtimestamp(((int(snowflake) / 4194304) + 1420070400000) / 1000)
@@ -8,3 +9,6 @@ def random_date_between(a, b):
     max = int((b - a).total_seconds())
     d = timedelta(seconds=randint(0, max))
     return a + d
+
+def querify(params):
+    return "?" + urllib.parse.urlencode(params)
