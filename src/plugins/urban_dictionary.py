489c5338a74833e396adcb8a8186f9ff3147a7f6
jtuu <joona.tuukkanen@gmail.com>
2018-03-24 18:47:57 +0200

    Added urban dictionary command.

---
+++ src/plugins/urban_dictionary.py
@@ -0,0 +1,45 @@
+import re
+import urllib.request
+import json
+from ..utils import querify
+from discord import Embed
+
+trigger = re.compile("^!ud")
+
+match_pattern = re.compile(r"^!ud\s+(\S+)")
+base_url = "http://api.urbandictionary.com/v0/define"
+
+def get_term(term):
+    params = {"term": term}
+    try:
+        response = urllib.request.urlopen(base_url + querify(params)).read()
+    except urllib.error.HTTPError as err:
+        print("Urban Dictionary request failed: %s" % err)
+    else:
+        try:
+            parsed = json.loads(response)
+        except json.JSONDecodeError as err:
+            print("Failed to parse Urban Dictionary response: %s" % err)
+        else:
+            if isinstance(parsed, dict):
+                terms = parsed.get("list")
+                if terms and isinstance(terms, list) and len(terms) > 0:
+                    best = terms[0]
+                    for term in terms:
+                        if term.get("thumbs_up") > best.get("thumbs_up"):
+                            best = term
+                    return best
+
+max_embed_field_length = 1024
+max_embed_footer_length = 2048
+
+async def action(bot, msg):
+    match = match_pattern.match(msg.clean_content)
+    if match:
+        term = get_term(match.groups()[0])
+        if term:
+            embed = Embed()
+            embed.set_footer(text=term["example"][:max_embed_footer_length])
+            await bot.send_message(msg.channel, content=term["definition"], embed=embed)
+        else:
+            await bot.send_message(msg.channel, "¯\\_(ツ)_/¯")
