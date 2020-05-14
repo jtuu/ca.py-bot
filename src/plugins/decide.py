29da123c0491d264b0a8c2534d1268a4add3396f
jtuu <joona.tuukkanen@gmail.com>
2020-05-14 04:55:51 +0300

    Fixed !decide command not parsing its arguments correctly.

--- src/plugins/decide.py
+++ src/plugins/decide.py
@@ -4,17 +4,11 @@ import random
 trigger = re.compile("^!decide")
 keywords = ["decide"]
 
-delim = "or"
-match_pattern = re.compile("^!decide\s(?!.*\s" + delim + "\s" + delim + ").+?\s" + delim + "\s(?!.*\s" + delim + ").+")
-split_pattern = re.compile("\s+" + delim + "\s+")
-
 async def action(bot, msg):
     """**!decide** _a_ **or** _b_ [**or** _c_] ...
 Helps you with decisions.
 `!decide to be or not to be`"""
-    msg_text = msg.clean_content
-    match = match_pattern.match(msg_text)
-    if match:
-        split = split_pattern.split(trigger.sub("", msg_text))
-        choice = random.choice(split).strip()
+    cmd_args = [arg.strip() for arg in trigger.sub("", msg.clean_content).split(" or ")]
+    if len(cmd_args) > 1:
+        choice = random.choice(cmd_args)
         await bot.send_message(msg.channel, choice)
